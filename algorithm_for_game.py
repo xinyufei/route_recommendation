eb58cd28124977802fdb0717f713af1881e946a9
Xinyu Fei <xyfei27@gmail.com>
2023-11-01 23:11:36 -0400

    Add files via upload

---
+++ algorithm_for_game.py
@@ -0,0 +1,243 @@
+import gurobipy as gp
+import networkx as nx
+
+
+def get_recommended_route(arc_set, origin_node, dest_node, num_nodes, bpr_params, trust, demand, capacity,
+                          free_flow_time, shortest_path, explore=None, eta=0, report_threshold=1,
+                          num_dangerous_reports=None):
+    """
+        This function solves the static model for the given arc set, origin node, destination node and number of nodes.
+        :param arc_set: available arc set at current time
+        :param origin_node: origin node set
+        :param dest_node: destination node set
+        :param num_nodes: total number of nodes
+        :param bpr_params: BPR function parameters
+        :param trust: trust value for each origin node (each player)
+        :param demand: demand for each origin node (each player)
+        :param capacity: capacity for each arc
+        :param free_flow_time: free flow time for each arc
+        :param shortest_path: shortest path for each origin node given the current arc set A
+        :param explore: explored rate of each arc (dangerous / not dangerous)?
+        :param eta: parameter for information gain
+        :param report_threshold: threshold for reports. If the number of dangerous reports is larger than this threshold,
+        the arc is considered as dangerous and should be deleted from arc_set
+        :param num_dangerous_reports: number of dangerous reports
+        :return:
+    """
+    # check if the dangerous reports are larger than the threshold:
+    # delete the arc from arc_set
+    if num_dangerous_reports is not None:
+        arc_set = {(i, j) for i, j in arc_set if num_dangerous_reports[i, j] <= report_threshold}
+    # update shortest path set
+    G = nx.DiGraph()
+    for i, j in arc_set:
+        G.add_edge(i, j, weight=free_flow_time[(i, j)])
+    # find the shortest path
+    shortest_path = {}
+    travel_time = {}
+    shortest_path_per_node = {}
+    travel_time_per_node = {}
+    for i in origin_node:
+        j_min = sum(free_flow_time.values())
+        for j in dest_node:
+            shortest_path[i, j] = nx.dijkstra_path(G, i, j)
+            travel_time[i, j] = nx.dijkstra_path_length(G, i, j)
+            if travel_time[i, j] < j_min:
+                j_min = travel_time[i, j]
+                shortest_path_per_node[i] = shortest_path[i, j]
+                travel_time_per_node[i] = travel_time[i, j]
+    # solve the static model
+    print("Solve the optimization model to get optimal recommendation with weight on exploration.")
+    m, x, z, obj1, obj2, x_val, z_val = solve_static_model(arc_set, origin_node, dest_node, num_nodes, bpr_params,
+                                                           trust, demand, capacity, free_flow_time,
+                                                           shortest_path_per_node, explore, eta)
+    print(x_val)
+    # find the recommended route
+    print("Find recommended route from optimized flow")
+    m_origin, x_origin = get_flow_per_origin(x_val, arc_set, origin_node, dest_node, num_nodes)
+    x_origin_val = {}
+    for o in origin_node:
+        x_origin_val[o] = m_origin.getAttr('x', x_origin[o])
+    # get recommended route
+    recommended_route = determine_path_flows(G, x_origin_val, origin_node, dest_node)
+    result = {}
+    result['planned_arc_flow_origin'] = x_origin_val
+    result['planned_total_time'] = obj1
+    result['planned_exploration_term'] = obj2
+    result['recommended_route'] = recommended_route
+    return result
+
+
+def solve_static_model(arc_set, origin_node, dest_node, num_nodes, bpr_params, trust, demand, capacity, free_flow_time,
+                       shortest_path, explore=None, eta=0):
+    '''
+    This function solves the static model for the given arc set, origin node, destination node and number of nodes.
+    :param arc_set: available arc set at current time
+    :param origin_node: origin node set
+    :param dest_node: destination node set
+    :param num_nodes: total number of nodes
+    :param bpr_params: BPR function parameters
+    :param trust: trust value for each origin node (each player)
+    :param demand: demand for each origin node (each player)
+    :param capacity: capacity for each arc
+    :param free_flow_time: free flow time for each arc
+    :param shortest_path: shortest path for each origin node given the current arc set A
+    :param explore: explored rate of each arc (dangerous / not dangerous)?
+    :param eta: parameter for information gain
+    :param report_threshold: threshold for reports. If the number of dangerous reports is larger than this threshold,
+    the arc is considered as dangerous and should be deleted from arc_set
+    :param num_dangerous_reports: number of dangerous reports
+    :return:
+    '''
+    m = gp.Model("static_model")
+    # Add variables by dictionary arc_set
+    x = m.addVars(arc_set, name="x")
+    z = m.addVars(arc_set, name="z")
+    # Add auxiliary variables
+    gamma = m.addVars(arc_set, name="gamma")
+    lambda_ = m.addVars(arc_set, name="lambda")
+    mu_ = m.addVars(arc_set, name="mu")
+    # Add objective function
+    obj1 = gp.quicksum(free_flow_time[i, j] * (z[i, j] + bpr_params[0] * gamma[i, j] / capacity[i, j] ** bpr_params[1])
+                       for i, j in arc_set)
+    obj2 = 0
+    # if we consider reports of blocked arcs, add an information gain term
+    weight = {}
+    if explore is not None:
+        for i, j in arc_set:
+            weight[i, j] = 1 / (explore[i, j] + 1e-3)
+            obj2 += (z[i, j] - weight[i, j] / sum(weight.values()) * sum(demand.values())) * (
+                    z[i, j] - weight[i, j] / sum(weight.values()) * sum(demand.values()))
+        obj2 = eta * obj2
+        print('Add flow variance objective')
+    m.setObjective(obj1 + obj2, gp.GRB.MINIMIZE)
+    # # Add SOCP constraints
+    socp_cons_1 = m.addConstrs((4 * z[i, j] * z[i, j] + (lambda_[i, j] - 1) * (lambda_[i, j] - 1) -
+                                (lambda_[i, j] + 1) * (lambda_[i, j] + 1) <= 0 for i, j in arc_set), name='socp_1')
+    socp_cons_2 = m.addConstrs((4 * lambda_[i, j] * lambda_[i, j] + (mu_[i, j] - z[i, j]) * (mu_[i, j] - z[i, j]) <=
+                                (mu_[i, j] + z[i, j]) * (mu_[i, j] + z[i, j]) for i, j in arc_set), name='socp_2')
+    socp_cons_3 = m.addConstrs((4 * mu_[i, j] * mu_[i, j] + (gamma[i, j] - z[i, j]) * (gamma[i, j] - z[i, j]) <=
+                                (gamma[i, j] + z[i, j]) * (gamma[i, j] + z[i, j]) for i, j in arc_set), name='socp_3')
+    # Flow conservation constraints
+    flow_cons_lb = m.addConstrs((gp.quicksum(x[i, j] for j in range(num_nodes) if (i, j) in arc_set) - gp.quicksum(
+        x[j, i] for j in range(num_nodes) if (j, i) in arc_set) >= -1e-5 for i in range(num_nodes)
+                                 if i not in origin_node + dest_node), name='flow_lb')
+    flow_cons_ub = m.addConstrs((gp.quicksum(x[i, j] for j in range(num_nodes) if (i, j) in arc_set) - gp.quicksum(
+        x[j, i] for j in range(num_nodes) if (j, i) in arc_set) <= 1e-5 for i in range(num_nodes)
+                                 if i not in origin_node + dest_node), name='flow_ub')
+    # demand constraints
+    demand_cons = m.addConstrs((gp.quicksum(x[i, j] for j in range(num_nodes) if (i, j) in arc_set) - gp.quicksum(
+        x[j, i] for j in range(num_nodes) if (j, i) in arc_set) == trust[i] * demand[i] for i in origin_node),
+                               name='origin')
+    # constraints between x and z
+    x_z_constraints = m.addConstrs((z[i, j] - x[i, j] == 0 for i, j in arc_set
+                                    if (i, j) not in list(shortest_path.keys())), name='total')
+    x_z_constraints_add = m.addConstrs(
+        (z[i, j] - x[i, j] == gp.quicksum((1 - trust[o]) * demand[o] for o in shortest_path[i, j])
+         for i, j in arc_set if (i, j) in list(shortest_path.keys())), name='add')
+    # solve the model
+    m.Params.LogToConsole = 0
+    m.Params.LogFile = 'algorithm_process.log'
+    m.Params.DualReductions = 0
+    m.Params.BarHomogeneous = 1
+    m.Params.BarConvTol = 1e-6
+    m.optimize()
+    # get the value
+    x_val = m.getAttr('x', x)
+    z_val = m.getAttr('x', z)
+    # get value of obj1
+    obj1_val = obj1.getValue()
+    # get value of obj2
+    if explore is not None:
+        obj2_val = obj2.getValue()
+    else:
+        obj2_val = 0
+    # return the solution
+    return m, x, z, obj1_val, obj2_val, x_val, z_val
+
+
+def get_flow_per_origin(flow, arc_set, origin_node, dest_node, num_nodes):
+    """
+    Get the flow per origin node
+    :param flow: flow of each arc, returned as x_val in the function solve_static_model
+    :param arc_set: current arc set
+    :param origin_node: origin node set
+    :param dest_node: destination node set
+    :param num_nodes: number of nodes
+    :return: flow for each origin node and each arc
+    """
+    # build a simple model
+    m = gp.Model("get_flow_per_origin")
+    # Add variables by dictionary arc_set
+    x = {}
+    for o in origin_node:
+        x[o] = m.addVars(arc_set, name=f"x{o}")
+    # Add objective function
+    m.setObjective(0, gp.GRB.MINIMIZE)
+    # Flow conservation constraints
+    flow_cons = m.addConstrs((gp.quicksum(x[o][o, j] - flow[o, j] for j in range(num_nodes) if (o, j) in arc_set)
+                              - gp.quicksum(x[o][j, o] - flow[j, o] for j in range(num_nodes) if (j, o) in arc_set)
+                              == 0 for o in origin_node), name='demand')
+    flow_cons_no_origin = m.addConstrs((gp.quicksum(x[o][i, j] for j in range(num_nodes) if (i, j) in arc_set) -
+                                        gp.quicksum(x[o][j, i] for j in range(num_nodes) if (j, i) in arc_set) == 0
+                                        for i in range(num_nodes) if i not in origin_node + dest_node
+                                        for o in origin_node), name='flow')
+    total_flow_cons_ub = m.addConstrs(
+        (gp.quicksum(x[o][i, j] for o in origin_node) <= flow[i, j] + 1e-4 for i, j in arc_set),
+        name='total_ub')
+    total_flow_cons_lb = m.addConstrs(
+        (gp.quicksum(x[o][i, j] for o in origin_node) >= flow[i, j] - 1e-4 for i, j in arc_set),
+        name='total_lb')
+    # solve the model
+    m.Params.LogToConsole = 0
+    m.Params.LogFile = 'algorithm_process.log'
+    m.Params.DualReductions = 0
+    m.optimize()
+    # return the solution
+    return m, x
+
+
+def determine_path_flows(G, arc_flows, origin_node, dest_node):
+    """
+    Determine path flows given arc flows.
+    :param G: directed graph
+    :param arc_flows: A dictionary where keys are arcs and values are their flows.
+    :param origin_node: origin node set
+    :param dest_node: destination node set
+    :return: path flows
+    """
+    path_flow = {}
+    for s in origin_node:
+        # Get all simple paths from s to t
+        paths = []
+        for t in dest_node:
+            paths += list(nx.all_simple_paths(G, source=s, target=t))
+        # Create a new model
+        m = gp.Model("path_flow_model")
+        # Create variables for path flows
+        x = m.addVars(len(paths), vtype=gp.GRB.CONTINUOUS, name="x")
+        # Set objective: This is arbitrary as we're mainly interested in constraints
+        m.setObjective(gp.quicksum(x[p] for p in range(len(paths))), gp.GRB.MAXIMIZE)
+        # Constraints based on arc flows
+        for path in paths:
+            if path == [5, 1, 0]:
+                print(paths.index(path))
+            if len(path) < 6:
+                print(path)
+        for (u, v), flow in arc_flows[s].items():
+            paths_with_arc = [p for p in range(len(paths)) if (u, v) in zip(paths[p], paths[p][1:])]
+            if u == 5 and v == 1:
+                print(paths_with_arc)
+                print(flow)
+            m.addConstr(gp.quicksum(x[p] for p in paths_with_arc) <= flow, f"arc_{u}_{v}")
+        m.Params.LogToConsole = 0
+        m.Params.LogFile = 'algorithm_process.log'
+        m.Params.DualReductions = 0
+        # Solve the model
+        m.optimize()
+        path_flow[s] = {}
+        for i in range(len(paths)):
+            # print(paths[i], x[i].x)
+            if x[i].x > 1e-3:
+                path_flow[s][tuple(paths[i])] = x[i].x
+    return path_flow
---
+++ bandit.py
@@ -0,0 +1,178 @@
+import static_model
+import numpy as np
+
+
+# define a bandit class with given trust rate
+class Bandit:
+    # initialize the bandit with given trust rate
+    def __init__(self, network_structure, trust_rate):
+        self.trust_rate_mean = trust_rate
+        self.trust_rate_std = 0.1
+        self.network_structure = network_structure
+
+        # assign values of network structure dictionary to variables
+        self.arc_set = self.network_structure['arc_set']
+        self.origin_node = self.network_structure['origin_node']
+        self.dest_node = self.network_structure['dest_node']
+        self.num_nodes = self.network_structure['num_nodes']
+        self.bpr_params = self.network_structure['bpr_params']
+        self.demand = self.network_structure['demand']
+        self.capacity = self.network_structure['capacity']
+        self.free_flow_time = self.network_structure['free_flow_time']
+        self.shortest_path = self.network_structure['shortest_path']
+
+        self.solution = {}
+        self.trust = {}
+
+    def get_solution(self):
+        trust_rate = self.trust_rate_mean
+        for i in self.origin_node:
+            self.trust[i] = trust_rate
+        m, x, z, obj, x_val, z_val = static_model.solve_static_model(
+            self.arc_set, self.origin_node, self.dest_node, self.num_nodes, self.bpr_params,
+            self.trust, self.demand, self.capacity, self.free_flow_time, self.shortest_path)
+        self.solution = {'m': m, 'x': x, 'z': z, 'obj': obj, 'x_val': x_val, 'z_val': z_val,
+                         'time': obj / sum(self.demand.values())}
+
+    # define function of pulling the arm
+    def pull(self, true_trust_rate):
+        # # generate a random trust rate with the given mean and std and truncate it to [0,1]
+        # trust_rate = np.random.normal(self.trust_rate_mean, self.trust_rate_std)
+        # if trust_rate < 0:
+        #     trust_rate = 0
+        # elif trust_rate > 1:
+        #     trust_rate = 1
+        # conduct out-of-sample test to get reward
+        true_trust = {}
+        for i in self.origin_node:
+            true_trust[i] = true_trust_rate
+        true_time_val = static_model.true_time(
+            self.solution['x_val'], self.bpr_params, self.free_flow_time, self.capacity, self.arc_set,
+            self.trust, true_trust, self.num_nodes, self.origin_node, self.dest_node, self.demand, self.shortest_path)
+        # return reward
+        return true_time_val / sum(self.demand.values())
+
+
+# define class of optimizer for bandit
+class GreedyOptimizer:
+    def __init__(self, network_structure, true_trust_rate, true_trust_rate_std=0.01, epsilon=0.05):
+        self.network_structure = network_structure
+        self.true_trust_rate = true_trust_rate
+        self.true_trust_rate_std = true_trust_rate_std
+        self.epsilon = epsilon
+        self.bandit_list = []
+
+    def build_bandit(self, trust_list):
+        for trust in trust_list:
+            trust = max(min(trust, 1), 0)
+            cur_bandit = Bandit(self.network_structure, trust)
+            cur_bandit.get_solution()
+            self.bandit_list.append(cur_bandit)
+
+    def get_optimal_travel_time(self):
+        trust = {}
+        for i in self.network_structure['origin_node']:
+            trust[i] = self.true_trust_rate
+        m, x, z, obj, x_val, z_val = static_model.solve_static_model(
+            self.network_structure['arc_set'], self.network_structure['origin_node'],
+            self.network_structure['dest_node'], self.network_structure['num_nodes'],
+            self.network_structure['bpr_params'], trust, self.network_structure['demand'],
+            self.network_structure['capacity'], self.network_structure['free_flow_time'],
+            self.network_structure['shortest_path'])
+        return obj / sum(self.network_structure['demand'].values())
+
+    def run(self, num_iter):
+        actions = []
+        travel_time = []
+        total_time_bandit = np.zeros(len(self.bandit_list))
+        pull_time_bandit = np.zeros(len(self.bandit_list))
+        opt_time = self.get_optimal_travel_time()
+        sample_trust_list = []
+        for i in range(num_iter):
+            # get trust rate by truncated normal distribution
+            trust_rate = np.random.normal(self.true_trust_rate, self.true_trust_rate_std)
+            trust_rate = max(min(trust_rate, 1), 0)
+            sample_trust_list.append(trust_rate)
+            # pull each arm first
+            if i < len(self.bandit_list):
+               # pull the arm
+                actions.append(i)
+                cur_time = self.bandit_list[i].pull(trust_rate)
+                total_time_bandit[i] += cur_time
+                pull_time_bandit[i] += 1
+                travel_time.append(cur_time)
+            else:
+                # get a probability of epsilon to pull a random arm
+                if np.random.random() < self.epsilon:
+                    # pull a random arm
+                    arm = np.random.randint(len(self.bandit_list))
+                    actions.append(arm)
+                    cur_time = self.bandit_list[arm].pull(trust_rate)
+                    total_time_bandit[arm] += cur_time
+                    pull_time_bandit[arm] += 1
+                    travel_time.append(cur_time)
+                else:
+                    # pull the arm with the largest average reward
+                    arm = np.argmin(total_time_bandit / pull_time_bandit)
+                    actions.append(arm)
+                    cur_time = self.bandit_list[arm].pull(trust_rate)
+                    total_time_bandit[arm] += cur_time
+                    pull_time_bandit[arm] += 1
+                    travel_time.append(cur_time)
+        return actions, travel_time, total_time_bandit, pull_time_bandit, opt_time, sample_trust_list
+
+    def run_ucb(self, num_iter, beta=1, reward='final'):
+        actions = []
+        travel_time = []
+        total_time_bandit = np.zeros(len(self.bandit_list))
+        time_bandit_diff = np.zeros(len(self.bandit_list))
+        time_bandit_iteration = np.zeros(num_iter)
+        pull_time_bandit = np.zeros(len(self.bandit_list))
+        opt_time = self.get_optimal_travel_time()
+        sample_trust_list = []
+        for i in range(num_iter):
+            # get trust rate by truncated normal distribution
+            trust_rate = np.random.normal(self.true_trust_rate, self.true_trust_rate_std)
+            trust_rate = max(min(trust_rate, 1), 0)
+            sample_trust_list.append(trust_rate)
+            # pull each arm first
+            if i < len(self.bandit_list):
+                # pull the arm
+                arm = i
+            else:
+                # compute the upper confidence bound
+                if reward == 'final':
+                    ucb = -total_time_bandit / pull_time_bandit + beta * np.sqrt(2 * np.log(i + 1) / pull_time_bandit)
+                elif reward == 'learn':
+                    ucb = -time_bandit_diff / pull_time_bandit + beta * np.sqrt(2 * np.log(i + 1) / pull_time_bandit)
+                # pull the arm with the largest upper confidence bound
+                arm = np.argmax(ucb)
+            actions.append(arm)
+            cur_time = self.bandit_list[arm].pull(trust_rate)
+            total_time_bandit[arm] += cur_time
+            time_bandit_diff[arm] += abs(self.bandit_list[arm].solution['time'] - cur_time)
+            time_bandit_iteration[i] = abs(self.bandit_list[arm].solution['time'] - cur_time)
+            pull_time_bandit[arm] += 1
+            travel_time.append(cur_time)
+            if (i + 1) % 1000 == 0:
+                print(f'At iteration {i + 1}, the cumulative average travel time is '
+                      f'{np.sum(travel_time) / (i + 1) * 0.6}, '
+                      f'and the regret is {(np.sum(travel_time) / (i + 1) - opt_time) * 0.6}')
+
+        # store all the results in a dictionary
+        result = {}
+        result['actions'] = actions
+        result['travel_time'] = travel_time
+        result['total_time_bandit'] = total_time_bandit
+        result['pull_time_bandit'] = pull_time_bandit
+        result['opt_time'] = opt_time
+        result['sample_trust_list'] = sample_trust_list
+        result['time_bandit_diff'] = time_bandit_diff
+        result['time_bandit_iteration'] = time_bandit_iteration
+        return result
+
+    # set the true trust rate
+    def set_true_trust_rate(self, true_trust_rate, true_trust_rate_std=0.01):
+        self.true_trust_rate = true_trust_rate
+        self.true_trust_rate_std = true_trust_rate_std
+
